338899778719e10a4b635c509e35027d9c70c1a5
AkifAhmedli <akifahmedli007@gmail.com>
2026-04-12 14:00:10 +0000

    rg

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Bu modul başqa bir funksiya qaytaran yüksək dərəcəli 
+funksiyanı ehtiva edir.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Multiplier qəbul edir və verilmiş rəqəmi həmin multiplier-ə
+    vuran yeni bir funksiya qaytarır.
+    """
+    def multiplier_func(n: float) -> float:
+        return n * multiplier
+
+    return multiplier_func
