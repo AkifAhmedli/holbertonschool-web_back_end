17748332d6ab0ed8f287a04269312aba0bcb7efe
AkifAhmedli <akifahmedli007@gmail.com>
2026-04-12 13:55:51 +0000

    rrg

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Bu modul bir float rəqəmi sətirə (string) çevirən
+funksiyanı ehtiva edir.
+"""
+
+
+def to_str(n: float) -> str:
+    """Verilmiş float rəqəmin sətir təmsilini qaytarır."""
+    return str(n)
