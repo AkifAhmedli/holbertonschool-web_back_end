c3fe26b3c492719c08b313d18f53fc428d5bcf22
AkifAhmedli <akifahmedli007@gmail.com>
2026-04-12 13:52:37 +0000

    rge

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Bu modul iki sətri birləşdirən funksiyanı ehtiva edir.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """İki sətri birləşdirir və nəticəni qaytarır."""
+    return str1 + str2
