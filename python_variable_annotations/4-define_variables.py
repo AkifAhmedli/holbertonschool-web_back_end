034db13095a7a8589c45ca62d8f73bce86884ba4
AkifAhmedli <akifahmedli007@gmail.com>
2026-04-12 13:56:40 +0000

    rhhg

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Bu modul müxtəlif tipli dəyişənləri tip təyinləri ilə birlikdə müəyyən edir.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
