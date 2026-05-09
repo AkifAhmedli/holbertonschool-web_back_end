12e52cc63d5b65a871b2494ba90deefb0a617b38
AkifAhmedli <akifahmedli007@gmail.com>
2026-05-09 19:02:09 +0000

    sfskgsrg

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python3
-"""
-MongoDB-də saxlanılan Nginx loqları haqqında statistikaları göstərən skript
-"""
+""" 12-log_stats """
 from pymongo import MongoClient
 
 
-def log_stats():
-    """
-    Nginx loqları üçün statistikaları çap edir
-    """
-    # MongoDB-yə qoşulma
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    # logs bazasının nginx kolleksiyası
-    collection = client.logs.nginx
-
-    # Ümumi loqların sayı
-    total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
-
-    # Metodlar üzrə statistika
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
-
-    # Xüsusi path üzrə yoxlama (GET /status)
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status_check))
+def _count(coll, query=None):
+    """Count documents with compatibility across PyMongo versions."""
+    if query is None:
+        query = {}
+    try:
+        return coll.count_documents(query)
+    except Exception:
+        return coll.count(query)
 
 
 if __name__ == "__main__":
-    log_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    print("{} logs".format(_count(collection)))
+    print("Methods:")
+    print("\tmethod GET: {}".format(_count(collection, {"method": "GET"})))
+    print("\tmethod POST: {}".format(_count(collection, {"method": "POST"})))
+    print("\tmethod PUT: {}".format(_count(collection, {"method": "PUT"})))
+    print("\tmethod PATCH: {}".format(_count(collection, {"method": "PATCH"})))
+    print("\tmethod DELETE: {}".format(_count(collection, {"method": "DELETE"})))
+    print("{} status check".format(
+        _count(collection, {"method": "GET", "path": "/status"})
+    ))
