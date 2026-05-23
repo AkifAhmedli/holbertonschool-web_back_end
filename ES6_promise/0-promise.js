04b9a76377ab32086f5600c103c41e3afb32edb3
AkifAhmedli <akifahmedli007@gmail.com>
2026-05-23 14:56:33 +0000

    Added

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    /* Hazırda daxilində heç bir məntiq tələb olunmur, 
+       sadəcə yeni bir Promise qaytarırıq */
+  });
+}
